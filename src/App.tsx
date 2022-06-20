5f65a504730062b05c085b88b7228bf3fd8cf29e
Blastose <blastoisejay@gmail.com>
2022-06-19 20:31:48 -0700

    Remove testing code

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,7 @@ import Header from "components/main-page/Header";
 import Footer from "components/main-page/Footer";
 import RawgApiWrapper from "rawg-api-wrapper/rawg-api-wrapper";
 
-async function asdf() {
-  // const apiWrapper = new RawgApiWrapper("a04bc9b870c3405d8f219c1fb7a9040e");
-  // const asdf = await apiWrapper.getGameScreenshots(3498);
-  // if (asdf) {
-  //   console.log(asdf);
-  // }
-}
 function App() {
-  asdf();
-
   const rawgApiWrapper = new RawgApiWrapper("a04bc9b870c3405d8f219c1fb7a9040e");
 
   return (
